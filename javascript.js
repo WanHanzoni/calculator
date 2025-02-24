c0a037ff1e22b42eeecf92d83fb0b4c4b4e533cc
juanan1996 <54101742+juanan1996@users.noreply.github.com>
2025-02-24 18:40:24 +0100

    Raise button add and use improve

--- javascript.js
+++ javascript.js
@@ -1,34 +1,33 @@
-
-function operatorAdd(numA, numB) {
-    return numA + numB
-}
-
-// console.log(operatorAdd(4, 5));
-
-// switch {
-//     case 1:
-//         console.log(buttonPressed());
-//         break;
-
-// }
-let display = document.getElementById("head");
+let result = document.getElementById("result");
+let display = document.getElementById("display");
 
 document.querySelectorAll("button").forEach(button => {
     button.addEventListener("click", function (event) {
         let button = event.target.textContent;
         let displayNumber = display.textContent.trim();
-        if ("0123456789./*-+".includes(button)) {
+
+        if ("0123456789.".includes(button)) {
             display.append(button);
+        } else if ("C".includes(button)) {
+            display.innerText = "";
+            result.innerText = "";
+        } else if ("=".includes(button)) {
+            let resultNumber = Function("return " + displayNumber + result.textContent.trim())();
+            display.innerText = "";
+            result.append(resultNumber);
+        } else if ("/*-+".includes(button)) {
+            display.append(result.textContent.trim())
+            display.append(button);
+            result.innerHTML = "";
+        } else if ("Raise".includes(button)) {
+            displayNumber = displayNumber.split("").slice(0, displayNumber.length - 1).join("");
+            display.innerHTML = "";
+            display.append(displayNumber);
         }
-        else if ("C".includes(button)) {
-            display.innerText = ""
-        }
-        else if ("=".includes(button)) {
-            var memoryNumber = displayNumber
-            display.innerText = ""
-            var result = Function("return " + memoryNumber)();
-            display.append(result);
+        if (display.textContent != "") {
+            result.style.visibility = "hidden";
+        } else if (display.textContent == "") {
+            result.style.visibility = "visible";
         }
     });
-
-});
+})
