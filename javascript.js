8c8e02c3423ec1d4f9a0727e55eb73f101ade52a
juanan1996 <54101742+juanan1996@users.noreply.github.com>
2025-02-24 12:27:02 +0100

    A functional Calculator but not least...

--- javascript.js
+++ javascript.js
@@ -1,7 +1,35 @@
-let memoryNumber = 0;
 
 function operatorAdd(numA, numB) {
     return numA + numB
 }
 
-console.log(operatorAdd(4, 5));
+// console.log(operatorAdd(4, 5));
+
+// switch {
+//     case 1:
+//         console.log(buttonPressed());
+//         break;
+
+// }
+let display = document.getElementById("head");
+
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", function (event) {
+        let button = event.target.textContent;
+        let displayNumber = display.textContent.trim();
+        if ("0123456789./*-+".includes(button)) {
+            display.append(button);
+        }
+        else if ("C".includes(button)) {
+            display.innerText = ""
+        }
+        else if ("=".includes(button)) {
+            var memoryNumber = displayNumber
+            display.innerText = ""
+            var result = Function("return " + memoryNumber)();
+            display.append(result);
+        }
+
+    });
+
+});
